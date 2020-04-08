dd076811ab896c6c3653946da6a353fc5ff4ec7b
redot <blackdulumi@gmail.com>
2020-04-09 01:44:00 +0900

    Site updated: 2020-04-09 01:44:00

--- PhotoSwipe/photoswipe-ui-default.js
+++ PhotoSwipe/photoswipe-ui-default.js
@@ -77,11 +77,13 @@ var PhotoSwipeUI_Default =
 			clickToCloseNonZoomable: true,
 
 			shareButtons: [
+/*
 				{id:'facebook', label:'Share on Facebook', url:'https://www.facebook.com/sharer/sharer.php?u={{url}}'},
 				{id:'twitter', label:'Tweet', url:'https://twitter.com/intent/tweet?text={{text}}&url={{url}}'},
 				{id:'pinterest', label:'Pin it', url:'http://www.pinterest.com/pin/create/button/'+
 													'?url={{url}}&media={{image_url}}&description={{text}}'},
-				{id:'download', label:'Download image', url:'{{raw_image_url}}', download:true}
+				{id:'download', label:'Download image', url:'{{raw_image_url}}', download:false}
+*/
 			],
 			getImageURLForShare: function( /* shareButtonData */ ) {
 				return pswp.currItem.src || '';
